ae3f1bbefec163f853c7a86fbde83de34ead7a32
Achyut Shukla <achyut.usc@gmail.com>
2017-04-23 11:42:40 -0700

    selection append insert remove

--- sample_1/script.js
+++ sample_1/script.js
@@ -13,4 +13,22 @@
 //d3.selectAll('.item:nth-child(2n)').text("Selecting every even element")
 
 // Selecting every odd element
-d3.selectAll('.item:nth-child(2n+1)').text("Selecting every odd element")
+//d3.selectAll('.item:nth-child(2n+1)').text("Selecting every odd element")
+
+// Selecting every 3rd element
+d3.selectAll('.item:nth-child(3n)').text("Selecting every 3rd element")
+
+// Modifying the html in the 2nd element
+d3.selectAll('.item:nth-child(2)').html('<strong>Strong Selection</strong>')
+
+// Appending a div to the 4th element
+d3.selectAll('.item:nth-child(4)').append('div').html('Adding value to 4th element')
+
+// Appending a span to the first item
+d3.select('.item').append('span').html(' <strong>Selection<strong>')
+
+// Inserting span element at the 3rd position
+d3.select('#chart').insert('span', ':nth-child(3)').html(' <strong>Inserting a span</strong>')
+
+// Removing the 2nd element
+d3.select('#chart .item:nth-child(2)').remove()
